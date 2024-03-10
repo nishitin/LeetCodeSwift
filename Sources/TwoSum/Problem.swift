c12aca865d54a3195cb153900e26aea94d420050
nisitin <seiya110307@icloud.com>
2024-03-10 18:18:23 +0900

    feat: TwoSum

--- Sources/TwoSum/Problem.swift
+++ Sources/TwoSum/Problem.swift
@@ -1,5 +1,12 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        
+        var numMap: [Int: Int] = [:] // 数値をキーとして、そのインデックスを値として格納するDictionary
+        for (i, num) in nums.enumerated() {
+            if let complementIndex = numMap[target - num] {
+                return [complementIndex, i]
+            }
+            numMap[num] = i
+        }
+        return []
     }
 }
