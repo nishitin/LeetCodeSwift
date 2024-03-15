1bce44fc8d7ed4d98dbc1f1bf05e252b64f956b4
nisitin <seiya110307@icloud.com>
2024-03-15 16:52:22 +0900

    feat: 回文判定

--- Sources/PalindromeNumber/Problem.swift
+++ Sources/PalindromeNumber/Problem.swift
@@ -1,8 +1,21 @@
-//
-//  File.swift
-//  
-//
-//  Created by 西聖也 on 2024/03/15.
-//
-
 import Foundation
+
+class PalindromeNumberSolution {
+    func isPalindrome(_ x: Int) -> Bool {
+        // 負の整数の場合と、10で割り切れる整数の場合は、回文にはならないためガード節で即Return
+        // 0は除く
+        if x < 0 || (x % 10 == 0 && x != 0) {
+            return false
+        }
+        
+        var num = x
+        var reversedNum = 0
+        
+        while num > reversedNum {
+            reversedNum = reversedNum * 10 + num % 10
+            num /= 10
+        }
+        // 引数で受け取った値と、引数で受け取った値を反転して比較する
+        return num == reversedNum || num == reversedNum / 10
+    }
+}
