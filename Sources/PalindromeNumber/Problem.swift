15b5fbff0098cc8fe0e6968b5ebd37b6dacff171
nisitin <seiya110307@icloud.com>
2024-03-15 15:15:15 +0900

    fix: 修正

---
+++ Sources/PalindromeNumber/Problem.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 西聖也 on 2024/03/15.
+//
+
+import Foundation
--- Sources/TwoSum/Problem.swift
+++ Sources/TwoSum/Problem.swift
@@ -1,4 +1,4 @@
-class Solution {
+class TwoSumSolution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var numMap: [Int: Int] = [:] // 数値をキーとして、そのインデックスを値として格納するDictionary
         for (i, num) in nums.enumerated() {
